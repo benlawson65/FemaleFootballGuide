0b0d2f126aa2138947bc7406fdfade1a174b6cb8
Homer Simpson <827377@swansea.ac.uk>
2017-01-29 13:48:35 +0000

    update project ready for transfer to github

--- FootbalAppSkeleton/FootbalAppSkeleton/LeagueWales.m
+++ FootbalAppSkeleton/FootbalAppSkeleton/LeagueWales.m
@@ -28,8 +28,10 @@ static NSMutableArray *allLeaguesWales;
 + (NSString *) getDataFromWales{
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     [request setHTTPMethod:@"GET"];
-    [request setURL:[NSURL URLWithString:@"https://www.kimonolabs.com/api/5tfl53ne?apikey=Zj1H9tsMUShsxu92JbWjbkhoaRIBxa4A"]];
+    //[request setURL:[NSURL URLWithString:@"https://www.kimonolabs.com/api/5tfl53ne?apikey=Zj1H9tsMUShsxu92JbWjbkhoaRIBxa4A"]];
     
+    [request setURL:[NSURL URLWithString:@"https://api.import.io/store/connector/4197fd3b-7c91-403c-9e76-95a61b149aeb/_query?format=JSON&input=%7Bteams%7D%3A%7Bpoints%7D%3A%7Bwins%7D%3A%7Bdraws%7D%3A%7Blosses%7D&_apikey=b8d1c620bb0f45829c91f9bdd88e104d8a221fc188b9bcaa1e6c6dea97764aa5eccacf8beea0c56608f514597cbbb97a51107c5a2058f7b85ea0d997ea70f853045cb071338cb6ed77dfc7ed551354d4"]];
+   
     NSError *error = [[NSError alloc] init];
     NSHTTPURLResponse *responseCode = nil;
     
@@ -49,8 +51,8 @@ static NSMutableArray *allLeaguesWales;
     
     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[returnedDataWales dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
     
-    NSDictionary *userinfo=[json valueForKey:@"results"];
-    NSArray *detailedUserInfo = [userinfo valueForKey:@"collection1"];
+    NSArray *userInfo=[json valueForKey:@"results"];
+    //NSArray *detailedUserInfo = [userinfo valueForKey:@"collection1"];
     NSDictionary *singleGameDetails;
     //NSDictionary *user1;
     NSInteger i = 0;
@@ -59,28 +61,28 @@ static NSMutableArray *allLeaguesWales;
     
     [self resetLeaguesWales];
     
-    if(userinfo != nil){
-        for( i = 0; i < [detailedUserInfo count]; i++ ) {
+    if(userInfo != nil){
+        for( i = 0; i < [userInfo count]; i++ ) {
             
             skey = [NSString stringWithFormat:@"%ld",(long)i];
             
-            singleGameDetails = [detailedUserInfo objectAtIndex:i];
+            singleGameDetails = [userInfo objectAtIndex:i];
             
             LeagueWales * newLeaguesWales
             = [[LeagueWales alloc] init];
             
-            newLeaguesWales.team = [singleGameDetails objectForKey:@"Team"];
+            newLeaguesWales.team = [singleGameDetails objectForKey:@"teams"];
             //newLeaguesWales.team = [user1 valueForKey:@"text"];
             
-            newLeaguesWales.points = [singleGameDetails objectForKey:@"Points"];
+            newLeaguesWales.points = [singleGameDetails objectForKey:@"points"];
             
-            newLeaguesWales.goalDifference = [singleGameDetails objectForKey:@"GoalDifference"];
+            newLeaguesWales.goalDifference = [singleGameDetails objectForKey:@"goaldifference"];
             
-            newLeaguesWales.wins = [singleGameDetails objectForKey:@"Wins"];
+            newLeaguesWales.wins = [singleGameDetails objectForKey:@"wins"];
             
-            newLeaguesWales.losses = [singleGameDetails objectForKey:@"Losses"];
+            newLeaguesWales.losses = [singleGameDetails objectForKey:@"losses"];
             
-            newLeaguesWales.draws = [singleGameDetails objectForKey:@"Draws"];
+            newLeaguesWales.draws = [singleGameDetails objectForKey:@"draws"];
             
             newLeaguesWales.index = [NSString stringWithFormat:@"%ld", (long)index];
             
